9d8cc288f6b50ecbd82d6fa6cb219ca569a126ae
KSVarun <ksvarun001@gmail.com>
2022-03-30 21:39:13 +0530

    removed comments

--- extension.js
+++ extension.js
@@ -9,19 +9,11 @@ const vscode = require('vscode');
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, extension "latest-pr" is now active!');
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
     'latest-pr.giveMeLatestPR',
     function () {
-      // The code you place here will be executed every time your command is executed
-
-      // Display a message box to the user
       const cp = require('child_process');
       vscode.window.withProgress(
         {
@@ -69,7 +61,6 @@ function activate(context) {
   context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
